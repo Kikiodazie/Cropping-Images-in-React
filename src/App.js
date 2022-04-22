ad3fc1decd8b4c4c627838281e40a5b90ec793f7
Kikiodazie <kiki.odazie@yahoo.com>
2022-04-22 17:24:04 +0100

    SetUp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from "@cloudinary/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// Import required actions, qualifiers and values.
+import {
+  crop,
+  thumbnail,
+  imaggaCrop
+} from "@cloudinary/url-gen/actions/resize";
+import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+import { face } from "@cloudinary/url-gen/qualifiers/focusOn";
+
+const App = () => {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME
+    }
+  });
+
+  const imageToCropToSpecifiedDimension = cld.image("store");
+  const imageToCropToThumbnail = cld.image("man");
+  const imageToCropWithImagga = cld.image("beach");
+
+  imageToCropToSpecifiedDimension.resize(crop().width(300).height(300));
+
+  imageToCropToThumbnail.resize(
+    thumbnail().width(300).height(300).gravity(focusOn(face()))
   );
-}
+
+  imageToCropWithImagga.resize(imaggaCrop());
+
+  return (
+    <>
+      <div>
+        <h2>Crop To Specified Dimension</h2>
+        <AdvancedImage cldImg={imageToCropToSpecifiedDimension} />
+      </div>
+
+      <br />
+
+      <div>
+        <h2>Thumbnail Crop</h2>
+        <AdvancedImage cldImg={imageToCropToThumbnail} />
+      </div>
+
+      <br />
+      <br />
+
+      <div>
+        <h2>Imagga Crop</h2>
+        <AdvancedImage cldImg={imageToCropWithImagga} />
+      </div>
+    </>
+  );
+};
 
 export default App;
